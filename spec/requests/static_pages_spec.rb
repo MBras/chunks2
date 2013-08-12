e2ed564737aed89c11ab8bcdf3b72fa3f7eacd7c
Mark Braspenning <mark.braspenning@gmail.com>
2013-08-12 23:22:37 +0200

    Adjusted static pages titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,50 @@
 require 'spec_helper'
 
-describe "Home page" do
+describe "Static pages" do
 
-  it "should have the content 'Chunks'" do
-    visit '/static_pages/home'
-    expect(page).to have_content('Chunks')
-  end
-	
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Chunks | Home")
+	let(:base_title) { "Chunks" }
+
+	describe "Home page" do
+
+		it "should have the content 'Chunks'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Chunks')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("#{base_title}")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
+		end
 	end
-end
 
-describe "Help page" do
+	describe "Help page" do
 
-  it "should have the content 'Chunks'" do
-    visit '/static_pages/help'
-    expect(page).to have_content('FAQ')
-  end
+		it "should have the content 'Chunks'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('FAQ')
+		end
 
-	it "should have the right title" do
-	  visit '/static_pages/help'
-	  expect(page).to have_title("Chunks | FAQ")
+		it "should have the right title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("#{base_title} | FAQ")
+		end
 	end
-end
 
-describe "About page" do
+	describe "About page" do
 
-  it "should have the content 'About Chunks'" do
-    visit '/static_pages/about'
-    expect(page).to have_content('About Chunks')
-  end
+		it "should have the content 'About Chunks'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About us')
+		end
 
-	it "should have the right title" do
-	  visit '/static_pages/about'
-	  expect(page).to have_title("Chunks | About us")
+		it "should have the right title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("#{base_title} | About us")
+		end
 	end
 end
